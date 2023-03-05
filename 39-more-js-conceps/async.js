426bf0a42de60841ecd8c91cfd18c0f4f12c00be
ali1924 <mohammadali229987@gmail.com>
2023-03-05 10:34:39 +0600

    Single-threaded, Asynchronous vs Synchronous

--- 39-more-js-conceps/async.js
+++ 39-more-js-conceps/async.js
@@ -1,1 +1,16 @@
 console.log(1);
+console.log(2);
+// doSomething();
+// setTimeout(doSomething);
+setTimeout(doSomething,4000);
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => console.log(json))
+console.log(3);
+console.log(4);
+
+function doSomething(){
+      console.log(5);
+}
+
+setTimeout(()=>console.log('Lettttttttttt'),4000);
